c8db6afd29dd3324add570ac24e4e545bb2a59d6
Claudinei B. Junior <claudineibjr@hotmail.com>
2017-06-30 14:24:11 -0300

    Questões aleatórioas 90% funcionando

--- src/model/Question.ts
+++ src/model/Question.ts
@@ -3,7 +3,7 @@ export class Question {
     private idQuestion: number;
     private question: string;
     private answer: number; /*0 - Alternativa A | 1 - Alternativa B | 2 - Alternativa C | 3 - Alternativa D*/
-    public alternatives: Array<String>;
+    private alternatives: Array<String>;
     private textBiblical: string;
     private levelQuestion: number;
     private testamento: string; /* Antigo | Novo */
@@ -26,13 +26,13 @@ export class Question {
         this.referenciaBiblica = referenciaBiblica;
     }
 
-    public getId(): number{                 return this.idQuestion; }
-    public getQuestion(): string{           return this.question; }
-    public getAnswer(): number{             return this.answer; }
-    public getAlternatives(): Array<String>{     return this.alternatives;   }
-    public getTextBiblical(): string{       return this.textBiblical;   }
-    public getLevelQuestion(): number{      return this.levelQuestion;  }
-    public getTestamento(): string{         return this.testamento; }
-    public getReferenciaBiblica(): string{  return this.testamento; }
+    public getId(): number{                     return this.idQuestion;     }
+    public getQuestion(): string{               return this.question;       }
+    public getAnswer(): number{                 return this.answer;         }
+    public getAlternatives(): Array<String>{    return this.alternatives;   }
+    public getTextBiblical(): string{           return this.textBiblical;   }
+    public getLevelQuestion(): number{          return this.levelQuestion;  }
+    public getTestamento(): string{             return this.testamento;     }
+    public getReferenciaBiblica(): string{      return this.testamento;     }
     
 }
