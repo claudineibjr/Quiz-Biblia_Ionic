eb03e3d6ff178ae761b6371f527a63c06f038c28
Claudinei Brito Junior <claudineibjr@hotmail.com>
2018-08-06 20:37:02 -0300

    Inserção das informações do usuário na aba perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { User } from '../../model/User';
+import { UserServices } from '../../services/UserServices';
 
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html'
 })
+
 export class ProfilePage {
+  user_Email: string;
+  user_Name: string;
+  user_Score: number;
+  user_AnsweredNumber: number;
+  
+  user_Preferences_Sound: boolean;
+  user_Preferences_Vibration: boolean;
+
+  user_Bonus_Alternative: number;
+  user_Bonus_BiblicalReference: number;
+  user_Bonus_Time: number;
+  user_Bonus_LastReceived: Date;
 
   constructor(public navCtrl: NavController) {
+    this.user_Email = UserServices.getUser().getEmail();
+    this.user_Name = UserServices.getUser().getName();
+    this.user_Score = UserServices.getUser().getScore();
+    this.user_AnsweredNumber = UserServices.getUser().getAnswered().length;
 
+    this.user_Preferences_Sound = UserServices.getUser().getPreferences().getSound();
+    this.user_Preferences_Vibration = UserServices.getUser().getPreferences().getVibration();
+
+    this.user_Bonus_Alternative = UserServices.getUser().getBonus().getAlternative();
+    this.user_Bonus_BiblicalReference = UserServices.getUser().getBonus().getBiblicalReference();
+    this.user_Bonus_Time = UserServices.getUser().getBonus().getTime();
+    this.user_Bonus_LastReceived = UserServices.getUser().getBonus().getLastBonusReceived();
+    console.log('user_Bonus_LastReceived');
+    console.log(this.user_Bonus_LastReceived);
   }
-
 }
